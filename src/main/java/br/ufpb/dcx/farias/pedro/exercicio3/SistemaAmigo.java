d31c118fbf2a3327240649e64ac7192851042418
Pedro <elpedro28006@gmail.com>
2026-05-26 22:10:48 -0300

    fazendo exercicio 3

--- src/main/java/br/ufpb/dcx/farias/pedro/exercicio3/SistemaAmigo.java
+++ src/main/java/br/ufpb/dcx/farias/pedro/exercicio3/SistemaAmigo.java
@@ -7,40 +7,53 @@ public class SistemaAmigo {
     private List<Mensagem> mensagens;
     private List<Amigo> amigos;
 
-
-    public void cadastrarAmigo(String nomeAmigo, String emailAmigo){
+    public SistemaAmigo() {
+        this.mensagens = new ArrayList<>();
+        this.amigos = new ArrayList<>();
     }
 
-    public Amigo pesquisaAmigo(String emailAmigo){
-        for(Amigo a : amigos){
-            if(a.getEmail().equals(emailAmigo)){
+    public void cadastrarAmigo(String nomeAmigo, String emailAmigo) {
+    }
+
+    public Amigo pesquisaAmigo(String emailAmigo) {
+        for (Amigo a : amigos) {
+            if (a.getEmail().equals(emailAmigo)) {
                 return a;
             }
         }
-    return null;
-}
-
-public void enviarMensagemParaTodos(String texto, String emailRemetente, boolean ehAnonima){
-}
-
-public void enviarMensagemParaAlguem(String texto, String emailRemetente, String emailDestinatario, boolean ehAnonima){
-}
-
-public List<Mensagem> pesquisaTodasAsMensagens(){
-    return this.mensagens;
-}
-
-public List<Mensagem> pesquisaMensagensAnonimas(){
-    List<Mensagem> anonimas = new ArrayList<>();
-
-    for(Mensagem mensagens : anonimas){
-
+        return null;
     }
-}
 
-public void configuraAmigoSecretoDe(String emailDaPessoa, String emailSorteado){
-}
+    public void enviarMensagemParaTodos(String texto, String emailRemetente, boolean ehAnonima) {
+    }
 
-public String pesquisaAmigoSecretoDe(String emailDaPessoa){
-    return
+    public void enviarMensagemParaAlguem(String texto, String emailRemetente, String emailDestinatario, boolean ehAnonima) {
+    }
+
+    public List<Mensagem> pesquisaTodasAsMensagens() {
+        return this.mensagens;
+    }
+
+    public List<Mensagem> pesquisaMensagensAnonimas() {
+        List<Mensagem> anonimas = new ArrayList<>();
+
+        for (Mensagem m : this.mensagens) {
+            if (m.ehAnonima()) {
+                anonimas.add(m);
+            }
+        }
+        return anonimas;
+    }
+
+
+    public void configuraAmigoSecretoDe(String emailDaPessoa, String emailSorteado) {
+    }
+
+    public String pesquisaAmigoSecretoDe(String emailDaPessoa) {
+        Amigo a = pesquisaAmigo(emailDaPessoa);
+        if (a != null) {
+            return a.getEmailAmigoSorteado();
+        }
+        return "Amigo não encontrado.";
+    }
 }
